66b57eee2c5e9b38bf02d1baba570ef6d145afbf
Alex13Ed <341724f@gmail.com>
2025-02-11 20:24:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// let currentHeight = window.innerHeight;
+// let currentWidth = window.innerWidth;
+
+// console.log(currentHeight);
+// console.log(currentWidth);
+// const wid = currentWidth * 0.3;
+// const hei = currentHeight * 0.3;
+
+// console.log(wid);
+// console.log(hei);
+
+// if (2*hei >= currentHeight) {
+
+// }
